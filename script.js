05b1886da4c9933d48c660b397feb78e61a4138f
samireddy yasaswini <147230794+yasaswini123-ai@users.noreply.github.com>
2025-03-03 14:39:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("signin-btn").addEventListener("click", function() {
+    document.getElementById("signform").style.display = "block";
+});
+
+document.querySelector(".close-btn").addEventListener("click", function() {
+    document.getElementById("signform").style.display = "none";
+});
